789b6bf2cfe4980e2a92e8c6e3a18701f317be67
Yash <yashghogre100@gmail.com>
2024-02-18 23:48:57 +0530

    Minor Changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,19 +4,19 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
+      <div className={styles.leftDiv}>
         <div>
           <h3 className={styles.mainText}>Your One Stop Solution to <span className={styles.span}>Fitness<Image src={'/underline.jpg'} height={500} width={500} alt="ul" className={styles.ulImg} /></span></h3>
         </div>
         <div>
-          <p>Transform Your Body, Ignite Your Strength: Unleash Your Potential at Body Temple!</p>
+          <p className={styles.tagline}>Embark on a Journey to Fitness Excellence: Discover Your Strength, Sculpt Your Body, and Elevate Your Wellness at Body Temple – Where Every Workout Transforms Lives!</p>
         </div>
-        <div>
-          <button>hello</button>
-          <button>hello</button>
+        <div className={styles.btnDiv}>
+          <button className={styles.btn1}>Get Free Trial</button>
+          <button className={styles.btn2}>Invite Friends</button>
         </div>
       </div>
-      <div>
+      <div className={styles.imgDiv}>
         <Image src={'/girlInGym.png'} height={500} width={500} alt="girl" className={styles.image} />
       </div>
     </main>
