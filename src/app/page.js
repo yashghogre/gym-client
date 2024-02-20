2b4ef894b170d30f81bcd5642f7bdb06d708f574
Yash <yashghogre100@gmail.com>
2024-02-20 19:23:55 +0530

    Added shapes to the background

--- src/app/page.js
+++ src/app/page.js
@@ -3,21 +3,26 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.leftDiv}>
-        <div>
-          <h3 className={styles.mainText}>Your One Stop Solution to <span className={styles.span}>Fitness<Image src={'/underline.jpg'} height={500} width={500} alt="ul" className={styles.ulImg} /></span></h3>
+    <main className={styles.mmDiv}>
+      <div className={`${styles.mainDiv} ${styles.waveDiv} ${styles.wave}`}>
+        <div className={styles.main}>
+          <div className={styles.leftDiv}>
+            <div>
+              <h3 className={styles.mainText}>Your One Stop Solution to <span className={styles.span}>Fitness<Image src={'/underline.jpg'} height={500} width={500} alt="ul" className={styles.ulImg} /></span></h3>
+            </div>
+            <div>
+              <p className={styles.tagline}>Embark on a Journey to Fitness Excellence: Discover Your Strength, Sculpt Your Body, and Elevate Your Wellness at Body Temple – Where Every Workout Transforms Lives!</p>
+            </div>
+            <div className={styles.btnDiv}>
+              <button className={styles.btn1}>Get Free Trial</button>
+              <button className={styles.btn2}>Invite Friends</button>
+            </div>
+          </div>
+          <div className={styles.imgDiv}>
+            <Image src={'/girlInGym.png'} height={500} width={500} alt="girl" className={styles.image} />
+          </div>
         </div>
-        <div>
-          <p className={styles.tagline}>Embark on a Journey to Fitness Excellence: Discover Your Strength, Sculpt Your Body, and Elevate Your Wellness at Body Temple – Where Every Workout Transforms Lives!</p>
-        </div>
-        <div className={styles.btnDiv}>
-          <button className={styles.btn1}>Get Free Trial</button>
-          <button className={styles.btn2}>Invite Friends</button>
-        </div>
-      </div>
-      <div className={styles.imgDiv}>
-        <Image src={'/girlInGym.png'} height={500} width={500} alt="girl" className={styles.image} />
+        
       </div>
     </main>
   );
--- src/app/pricing/page.js
+++ src/app/pricing/page.js
@@ -4,7 +4,7 @@ import PricingCard from '../components/PricingCard'
 
 const page = () => {
     return (
-        <div className={styles.mainDiv}>
+        <div className={`${styles.mainDiv} ${styles.waveDiv} ${styles.wave}`}>
             <div className={styles.cardsDiv}>
                 <PricingCard name='Monthly' fkprice='1000' price='700' />
                 <PricingCard name='Quarterly' fkprice='3000' price='2000' mostBuy = 'true' />
