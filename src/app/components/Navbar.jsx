de634fba3113f75c9d84c2b17b99cdac6308ac28
Yash <yashghogre100@gmail.com>
2024-03-01 16:35:15 +0530

    Minor design changes

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '@/styles/Navbar.module.css'
 import Link from 'next/link'
 import { IoMdMenu } from "react-icons/io";
 
 const Navbar = () => {
+
+    const [menuDisplay, setMenuDisplay] = useState('none')
+
+    const menuClick = () => {
+        
+    }
+
     return (
         <div className={styles.mainDiv}>
             <div className={styles.titleDiv}>
@@ -28,8 +35,16 @@ const Navbar = () => {
             <div className={styles.btnDiv}>
                 <button className={styles.btn}>Join Now!</button>
             </div>
-            <div className={styles.menuDiv}>
+            <div className={styles.menuDiv} onClick={menuClick}>
                 <IoMdMenu size={50} color='#3559E0' />
+            </div>
+            <div className={styles.menuOptDiv}>
+                <ul>
+                    <li><Link href={'/#features'}>Features</Link></li>
+                    <li><Link href={'/pricing'}>Pricing</Link></li>
+                    <li><Link href={'/#testimonials'}>Testimonials</Link></li>
+                    <li><Link href={'/#location'}>Contact Us!</Link></li>
+                </ul>
             </div>
         </div>
     )
