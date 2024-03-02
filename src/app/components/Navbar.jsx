4988719979f90800fd6e9a4a0a403cbdc4927a3c
Yash <yashghogre100@gmail.com>
2024-03-02 20:36:41 +0530

    Made the navbar responsive menu

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,14 +1,33 @@
+'use client'
+
 import React, { useState } from 'react'
 import styles from '@/styles/Navbar.module.css'
 import Link from 'next/link'
 import { IoMdMenu } from "react-icons/io";
+import { useRouter } from 'next/navigation';
+import { motion } from "framer-motion"
 
 const Navbar = () => {
 
     const [menuDisplay, setMenuDisplay] = useState('none')
+    const router = useRouter()
 
     const menuClick = () => {
-        
+        setMenuDisplay((prev) => {
+            if (prev == 'none') {
+                setMenuDisplay('block')
+            }
+            else {
+                setMenuDisplay('none')
+            }
+
+            // setMenuDisplay('none')
+        })
+    }
+
+    const pushPricing = () => {
+        router.push('/pricing')
+        setMenuDisplay('none')
     }
 
     return (
@@ -38,12 +57,12 @@ const Navbar = () => {
             <div className={styles.menuDiv} onClick={menuClick}>
                 <IoMdMenu size={50} color='#3559E0' />
             </div>
-            <div className={styles.menuOptDiv}>
-                <ul>
-                    <li><Link href={'/#features'}>Features</Link></li>
-                    <li><Link href={'/pricing'}>Pricing</Link></li>
-                    <li><Link href={'/#testimonials'}>Testimonials</Link></li>
-                    <li><Link href={'/#location'}>Contact Us!</Link></li>
+            <div className={styles.menuOptDiv} style={{ display: menuDisplay }}>
+                <ul className={styles.list}>
+                    <li className={styles.listLink} onClick={() => { router.push('/#features'); setMenuDisplay('none'); }}>Features</li>
+                    <li className={styles.listLink} onClick={pushPricing}>Pricing</li>
+                    <li className={styles.listLink} onClick={() => { router.push('/#testimonials'); setMenuDisplay('none'); }}>Testimonials</li>
+                    <li className={styles.listLink} onClick={() => { router.push('/#location'); setMenuDisplay('none'); }}>Contact Us!</li>
                 </ul>
             </div>
         </div>
